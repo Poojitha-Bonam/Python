483aec0b6648fd25199aa388e80594acd4f8771b
Poojitha-Bonam <poojithabonam@gmail.com>
2025-01-23 14:12:48 +0530

    python-23-01-2025.py

--- python-23-01-2025.py
+++ python-23-01-2025.py
@@ -1,5 +1,4 @@
 #complex numbers
-
 a=5+8j
 b=8+5j
 print(a+b)
@@ -11,7 +10,6 @@ print(a**b)
 #print(a//b) #It shows TypeError: unsupported operand type(s) for //: 'complex' and 'complex'
 
 #Booleans - True or False
-
 a=294
 print(a==293)
 print(a==294)
@@ -24,8 +22,6 @@ print(int(True))
 #Sequences - Strings, List, Tuples, Range
 
 #List- List is a ordered collection of data items and can be changeable (Mutable)
-
-
 list=[0,3,4,'String',True,[1,"Hello",3]]
 print(id(list)) #for memory address
 print(type(list))
@@ -47,7 +43,6 @@ print(list[5])
 print(len(list[5][1])) #length of "hello" insode inner list
 
 #Tuple - Tuple is ordered collection of data items but unchangeable (Immutable)
-
 tuple=(1,2,3,4.5,[1,2,3],"String")
 print(tuple)
 print(id(tuple)) #for memory address
@@ -60,10 +55,9 @@ print(tuple[-4])
 #print(tuple) #It shows TypeError: 'tuple' object does not support item assignment, because tuple is immutable(cannot be change)
 
 #Range - Limit
-
-#print(list(range(0,100))) #To change range into list
-#print(list(range(1,100,2))) #2 refers ot step, it skips "1 index"
-#print(list(range(100,1,-1))) #Negative indexing
+print(list(range(0,100))) #To change range into list
+print(list(range(1,100,2))) #2 refers ot step, it skips "1 index"
+print(list(range(100,1,-1))) #Negative indexing
 
 
 
