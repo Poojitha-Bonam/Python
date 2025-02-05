c9b27c7f1c2a3f4016c7efb54f5dbef571936aa1
Poojitha-Bonam <poojithabonam@gmail.com>
2025-02-05 12:32:37 +0530

    task-2

--- task-2.py
+++ task-2.py
@@ -1,19 +1,19 @@
-# # 1.To print 1 to 100 numbers using while loop
-# n=0
-# while n<100:
-#     n=n+1
-#     print(n)
+# 1.To print 1 to 100 numbers using while loop
+n=0
+while n<100:
+    n=n+1
+    print(n)
 
-# # 2.To print 1 to 100 numbers in reverse order using for loop and while loop
-# # Using while loop
-# n=101
-# while n>0:
-#     n=n-1
-#     print(n)
+# 2.To print 1 to 100 numbers in reverse order using for loop and while loop
+# Using while loop
+n=101
+while n>0:
+    n=n-1
+    print(n)
 
-# # Using for loop
-# for i in range(100,0,-1):
-#     print(i)
+# Using for loop
+for i in range(100,0,-1):
+    print(i)
 
 # 3.To reverse a number using while loop without string conversion input:1234, output:4321
 num=1234
